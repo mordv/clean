aef589715ed06d2a17848ae0539e47d664a07c64
Alexander Mordvinov <32086218+mordv@users.noreply.github.com>
2024-03-28 14:55:47 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { default as viteReact } from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
-// import eslintPlugin from 'vite-plugin-eslint'; fuck eslint until they fix they're false-positive errors
 
 // https://vitejs.dev/config/
 export default defineConfig({
