24bb808c440a63550c25afa38da7b9586e1af0ad
alex <mwwalex@gmail.com>
2025-03-26 19:40:20 +0700

    wouter up;

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Redirect, Route, Router, Switch } from 'wouter';
-import { navigate, useLocationProperty } from 'wouter/use-location';
-import { HomePage } from '../pages/HomePage';
-import { Page } from '../pages/Page';
-
-const hashLocation = () => window.location.hash.replace(/^#/, '') || '/';
-const hashNavigate = (to: string) => navigate('#' + to);
-const useHashLocation = () => {
-  const location = useLocationProperty(hashLocation);
-  return [location, hashNavigate];
-};
+import { useHashLocation } from 'wouter/use-hash-location';
+import { HomePage } from '@/pages/HomePage';
+import { Page } from '@/pages/Page';
 
 export const AppRoutes: React.FC = () => (
   <Router hook={useHashLocation as any}>
     <Switch>
       <Route path="/:id">{(a) => <Page pageId={a.id} />}</Route>
+      <Route path="/nested" nest>
+        <Route path="/router">
+          <HomePage />
+        </Route>
+      </Route>
       <Route path="/">
         <HomePage />
       </Route>
